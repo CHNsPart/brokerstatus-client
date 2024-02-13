1811c7e3957626c8d71dc4ff22786ab374cd4343
chnspart <imchn24@gmail.com>
2024-02-12 23:57:59 -0500

    Day 18: Vercel config

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import { exampleNewsData } from './lib/utils';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Signin from './pages/Signin';
 import LatesNews from './components/LatesNews';
